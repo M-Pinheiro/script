bcbcbbc4aeb66058b1a42d45c980309f0de510d4
Marco <marco.pinheiro@postgrad.manchester.ac.uk>
2013-11-09 17:41:12 +0000

    samtool sort SRR4977*

--- samtoolssort-indexSRR.sh
+++ samtoolssort-indexSRR.sh
@@ -3,4 +3,44 @@
 # Script to run samtools indxstats on Wolbachia +ve and Wolbachia -ve samples to compare against Cell lines test using idxstats output.
 # Run this script as qsub.
 
-samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR4977*.bam ~/scratch/Wolbachia/SRR4977*.sorted
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497710.bam ~/scratch/Wolbachia/SRR497710.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497711.bam ~/scratch/Wolbachia/SRR497711.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497712.bam ~/scratch/Wolbachia/SRR497712.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497714.bam ~/scratch/Wolbachia/SRR497714.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497715.bam ~/scratch/Wolbachia/SRR497715.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497716.bam ~/scratch/Wolbachia/SRR497716.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497717.bam ~/scratch/Wolbachia/SRR497717.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497718.bam ~/scratch/Wolbachia/SRR497718.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497719.bam ~/scratch/Wolbachia/SRR497719.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497720.bam ~/scratch/Wolbachia/SRR497720.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497721.bam ~/scratch/Wolbachia/SRR497721.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497722.bam ~/scratch/Wolbachia/SRR497722.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497723.bam ~/scratch/Wolbachia/SRR497723.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497723.bam ~/scratch/Wolbachia/SRR497723.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497724.bam ~/scratch/Wolbachia/SRR497724.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497725.bam ~/scratch/Wolbachia/SRR497725.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497726.bam ~/scratch/Wolbachia/SRR497726.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497727.bam ~/scratch/Wolbachia/SRR497727.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497728.bam ~/scratch/Wolbachia/SRR497728.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497729.bam ~/scratch/Wolbachia/SRR497729.sorted
+
+samtools sort /mnt/lustre/scratch/mqbpjfg2/Data/hologenome/runs/SRR497730.bam ~/scratch/Wolbachia/SRR497730.sorted
