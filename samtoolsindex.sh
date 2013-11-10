c0e9ab60c1dc0eff7c3141f0affccb4bab9c4eb7
Marco <marco.pinheiro@postgrad.manchester.ac.uk>
2013-11-10 00:09:47 +0000

    index sorted SRR4977*

---
+++ samtoolsindex.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script to run samtools indxstats on SRR4977* samples to compare against Cell lines test using idxstats output.
+# Run this script as sh qsub.
+
+samtools index ~/scratch/Wolbachia/SRR497710.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497711.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497712.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497714.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497715.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497716.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497717.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497718.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497719.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497720.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497721.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497722.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497723.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497724.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497725.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497726.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497727.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497728.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497729.sorted.bam
+samtools index ~/scratch/Wolbachia/SRR497730.sorted.bam
